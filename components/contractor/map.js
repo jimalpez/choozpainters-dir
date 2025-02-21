c7a1671380f40960c47933ea0784e764502c3071
Jim Alpez <jim.alpez06@gmail.com>
2025-02-21 21:38:17 +0800

    update

--- components/contractor/map.js
+++ components/contractor/map.js
@@ -25,7 +25,7 @@ export class ContractorMap extends HTMLElement {
     }
 
     const mapOptions = {
-      zoom: 8, // Increased zoom for a more localized view
+      zoom: 2, // Increased zoom for a more localized view
       center: new google.maps.LatLng(this.latitude, this.longitude),
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       mapTypeControl: false,
